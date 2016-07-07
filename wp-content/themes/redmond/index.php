5e607767eda871b1d8d0c8b35e663698021d72a5
Robert M. Parker <rparker@yamiko.ninja>
2016-07-07 12:15:17 -0700

    start laying out templates

--- wp-content/themes/redmond/index.php
+++ wp-content/themes/redmond/index.php
@@ -2,4 +2,40 @@
 require_once __DIR__.'/App/bootstrap.php';
 
 $twig = $container->get("twig.environment");
-echo $twig->render('basic.html.twig');
+$data = [];
+$template = 'page.html.twig';
+
+setup_postdata($post);
+$flexibleContent = [];
+while(have_rows('content')) {
+    the_row();
+    switch($layout = get_row_layout()) {
+        case 'large_header':
+            $flexibleContent[] = $twig->render('modules/large-header.html.twig', $data);
+            break;
+        case 'title_section':
+            $flexibleContent[] = $twig->render('modules/title-section.html.twig', $data);
+            break;
+        case 'icon_columns':
+            $flexibleContent[] = $twig->render('modules/icon-columns.html.twig', $data);
+            break;
+        case 'partners':
+            $flexibleContent[] = $twig->render('modules/partners.html.twig', $data);
+            break;
+        case 'call_to_action':
+            $flexibleContent[] = $twig->render('modules/call-to-action.html.twig', $data);
+            break;
+        case 'comparison_table':
+            $flexibleContent[] = $twig->render('modules/table.html.twig', $data);
+            break;
+        case 'testimonial_slider':
+            $flexibleContent[] = $twig->render('modules/reviews-slider.html.twig', $data);
+            break;
+        default:
+            throw new Exception('Could not render layout for '.$layout);
+            break;
+    }
+}
+$data['flexible_content'] = $flexibleContent;
+
+echo $twig->render($template, $data);
